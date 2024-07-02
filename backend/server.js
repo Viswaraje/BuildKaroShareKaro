f1a320bae152582a32e388216554cc65a8fc6c74
viswaraje <v.raje2004@gmail.com>
2024-07-02 16:36:47 +0530

    package.json updated and server.js updated

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv"
 import cors from "cors"
 import passport from 'passport'
 
-
+import path from "path";
 import "./passport/github.auth.js"
 import session from "express-session";
 dotenv.config()
 
 const app=express()
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
@@ -17,10 +19,7 @@ app.use(passport.session());
 app.use(cors());
 
 
-app.get("/",(req,res)=>{
 
-    res.send("Server is ready")
-})
 
 //middleware
 
@@ -46,18 +45,21 @@ app.use("/api/explore",exploreRoutes)
 
 
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
 
 
-
-
-app.listen(5000,()=>{
-
-    console.log("Server started on http://localhost:5000")
-    connectMongoDB();
-})
+app.listen(PORT, () => {
+	console.log(`Server started on http://localhost:${PORT}`);
+	connectMongoDB();
+});
 
 
 
